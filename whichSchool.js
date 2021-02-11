07a69fd5e9e381b56fe01a3c96155b72e0161823
Trevor <tbowesy@hotmail.com>
2021-02-11 19:24:00 +0000

    which school proper line up

--- whichSchool.js
+++ whichSchool.js
@@ -1,9 +1,9 @@
-function whichSchool(age) {
+const whichSchool  = function (age) {
   if (age < 13) {
-  return "Elementary School";
-} else if (age <= 18) {
-  return "Secondary School";
-} else
+    return "Elementary School";
+  } else if (age <= 18) {
+    return "Secondary School";
+  } else
   return "Lighthouse Labs";
-};
+}
 console.log(whichSchool(42));
