1828e3b420e6570199d304cd74b6fbaa10f390a6
Trevor <tbowesy@hotmail.com>
2021-02-11 18:51:47 +0000

    logical Operators

---
+++ logicalOps.js
@@ -0,0 +1,19 @@
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+
+const temperature = -45
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea..");
+  }
+
+
+
+  const raining = false
+  if (!raining) {
+    console.log("Leave your umbrella at home!");
+  }
